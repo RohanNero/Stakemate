06dcb520378af4aa6099e9f100d9cc2d45be8cfb
ferric <ferric-sol@ferric.net>
2023-03-02 22:16:56 -0700

    New service to grab a withdrawal address and write to a file

---
+++ ssv-keyshare-service/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require('fs');
+const crypto = require('crypto');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const createValidatorConfig = (withdrawal_address) => {
+    let validatorConfig = {
+        "validator_count": 1,
+        "withdrawal_creds": withdrawal_address,
+        "keystore_password": ""
+    }
+    let data = JSON.stringify(validatorConfig);
+    fs.writeFileSync(`${withdrawal_address}-validator_config.json`, data);
+    
+}
+
+app.post('/create-keys', (req, res) => {
+    console.log(req.body.withdrawal_address);
+    createValidatorConfig(req.body.withdrawal_address);
+    res.sendStatus(200);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
