7d0b0571e4c57a28f72f1a69f93c1ed4419e80a2
Rohan <RohanZarathustra@gmail.com>
2023-03-15 19:27:18 -0400

    fs reading from json

--- scripts/depositValidatorPool.js
+++ scripts/depositValidatorPool.js
@@ -3,7 +3,7 @@ const {
   networkConfig,
   developmentChains,
 } = require("../helper-hardhat-config.js");
-const fs = require("fs");
+const fs = require("fs-extra");
 
 /** this function deposits validator keys for specified contract address and config file
  * ( will need to use hardhat task to pass parameters)
@@ -15,24 +15,36 @@ async function depositValidator() {
   //console.log("stakeValue:", stakeValue);
   //console.log("chainId:", chainId);
   const StakingPool = await ethers.getContract("StakingPoolV1");
+  //console.log("stakingPool:", StakingPool.address);
   // const abi = fs.readFile("./abi/StakingPoolV1.json", function (err, data) {
   //   console.log(data);
   // });
-  const depositData = fs.readdirSync("./keys/validator_keys", {
+  const depositDataFolder = fs.readdirSync("./keys/validator_keys", {
     encoding: "utf8",
   });
-  const test = depositData[0];
-  console.log(depositData);
+  const test = depositDataFolder[0];
+  //console.log(depositDataFolder);
+  let path;
   //console.log(test);
-  for (let i = 0; i < depositData.length; i++) {
-    if (depositData[i].includes("deposit_data")) {
-      console.log(depositData[i]);
-      const path = "./keys/validator_keys/" + depositData[i];
-      console.log(path);
+  for (let i = 0; i < depositDataFolder.length; i++) {
+    if (depositDataFolder[i].includes("deposit_data")) {
+      //console.log(depositData[i]);
+      path = "./keys/validator_keys/" + depositDataFolder[i];
+      //console.log("path:", path);
     }
   }
-
-  //console.log("stakingPool:", StakingPool.address);
+  const depositData = fs.readJsonSync(path, { encoding: "utf8" });
+  console.log(depositData);
+  //console.log("depositData length:", depositData.length);
+  for (let i = 0; i < depositData.length; i++) {
+    const args = [
+      depositData[i].pubkey,
+      depositData[i].withdrawal_credentials,
+      depositData[i].signature,
+      depositData[i].deposit_message_root,
+    ];
+    console.log(args);
+  }
 }
 
 depositValidator()
